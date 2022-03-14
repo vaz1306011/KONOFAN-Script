ad4368db901d0a1848efbccf8be3875bbf5398b2
vaz1306011 <vaz1306011@gmail.com>
2022-03-14 22:53:05 +0800

    新增程式結束處理
    新增程式結束處理

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from msilib.schema import Error
+
+
 def main():
     from konosubaScript import KonosubaScript
     ks = KonosubaScript()
-    key = input('(1)刷活動小關卡 (2)刷活動boss (3)刷競技場:')
-    if key == '0':
-        ks.goHome()
-    elif key == '1':
-        ks.eventAdventureLoop()
-    elif key == '2':
-        firstDelay = input('首次延遲(直接Enter=60s):')
-        ks.eventBossLoop(firstDelay)
-    elif key == '3':
-        ks.battleArenaLoop()
-    else:
-        print("輸入錯誤")
+    try:
+        key = input('(1)刷活動小關卡 (2)刷活動boss (3)刷競技場:')
+        if key == '0':
+            ks.goHome()
+        elif key == '1':
+            ks.eventAdventureLoop()
+        elif key == '2':
+            firstDelay = input('首次延遲(直接Enter=60s):')
+            ks.eventBossLoop(firstDelay)
+        elif key == '3':
+            ks.battleArenaLoop()
+        else:
+            print("輸入錯誤")
+    except KeyboardInterrupt:
+        print('程式結束')
 
 
 if __name__ == "__main__":
