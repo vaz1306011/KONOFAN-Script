7bcd29585bf48dfb06abd4c5048bcda0370cce12
vaz1306011 <vaz1306011@gmail.com>
2022-03-29 22:23:46 +0800

    操作優化
    (2)刷活動boss預設延遲改為0

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
     try:
         key = input('(1)刷活動小關卡 (2)刷活動boss (3)刷競技場:')
         if key == '0':
-            ks.goHome()
+            ks._click('go')
         elif key == '1':
             ks.eventAdventureLoop()
         elif key == '2':
-            firstDelay = input('首次延遲(直接Enter=60s):')
+            firstDelay = input('首次延遲(直接Enter=0s):')
             ks.eventBossLoop(firstDelay)
         elif key == '3':
             ks.battleArenaLoop()
