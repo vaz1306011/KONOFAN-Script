316227ad8151a8f870ec5fcdb54ba339de6c4662
vaz1306011 <vaz1306011@gmail.com>
2022-05-30 02:40:37 +0800

    修改排版
    加一些空行

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 def main():
     print('正在載入模組...')
-    import konofanScript as ks
     from os import system
+
+    import konofanScript as ks
     system('cls')
     ks.op.setPicPath('pic_address.json')
     try:
         key = input('(1)刷活動小關卡 (2)刷活動boss (3)刷競技場:')
         if key == '0':
             pass
+
         elif key == '1':
             ks.eventAdventureLoop()
+
         elif key == '2':
             firstDelay = input('首次延遲(直接Enter=0s):')
             ks.eventBossLoop(firstDelay)
+
         elif key == '3':
             ks.battleArenaLoop()
+
         else:
             print("輸入錯誤")
+
     except KeyboardInterrupt:
         print('程式結束')
 
