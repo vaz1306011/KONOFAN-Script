e63b60c8e0c151ac1a3c492608edf94ffaebd7f8
vaz1306011 <vaz1306011@gmail.com>
2022-07-08 20:35:02 +0800

    修改刷活動Boss提示

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import konofanScript as ks
+
+
 def main():
     print('正在載入模組...')
-    from os import system
-
-    import konofanScript as ks
-    system('cls')
-    ks.op.setPicPath('pic_address.json')
     try:
-        key = input('(1)刷活動小關卡 (2)刷活動boss (3)刷競技場:')
+        key = input('(1)刷活動小關卡 (2)刷活動Boss (3)刷競技場:')
         if key == '0':
-            pass
+            ...
 
         elif key == '1':
             ks.eventAdventureLoop()
 
         elif key == '2':
-            firstDelay = input('首次延遲(直接Enter=0s):')
+            firstDelay = input('首次延遲(預設0秒):')
             ks.eventBossLoop(firstDelay)
 
         elif key == '3':
