af3c7c42af7eaf00211bf8c08cc057dccf35323b
vaz1306011 <vaz1306011@gmail.com>
2022-08-11 16:59:11 +0800

    改為用black排版

--- operate.py
+++ operate.py
@@ -1,6 +1,7 @@
 import json
 import threading
 from collections import namedtuple
+from functools import partial
 from pathlib import Path
 from time import perf_counter, sleep
 from typing import Iterable, Union
@@ -9,10 +10,9 @@ import pyautogui as pag
 import win32api
 import win32con
 import win32gui
-
-# 不知道什麼原理,反正這3條讓我可以在副螢幕上用這個腳本
 from PIL import ImageGrab
-from functools import partial
+
+# 讓螢幕截圖可以用在所有螢幕上
 ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 
 FAILSAFE: bool = True  # 安全防護
@@ -30,11 +30,11 @@ class ExitEventException(Exception):
 
 
 class NamedPoint:
-    '''
+    """
     帶名座標類
-    '''
+    """
 
-    def __init__(self, pos: Union['NamedPoint', Iterable], name=None) -> None:
+    def __init__(self, pos: Union["NamedPoint", Iterable], name=None) -> None:
         try:
             self.x, self.y = int(pos.x), int(pos.y)
         except AttributeError:
@@ -43,59 +43,56 @@ class NamedPoint:
         self.name = str(name)
 
     def __str__(self) -> str:
-        return f'{self.name}:({self.x}, {self.y})'
+        return f"{self.name}:({self.x}, {self.y})"
 
-    def __add__(self, other: Union['NamedPoint', Iterable]) -> 'NamedPoint':
+    def __add__(self, other: Union["NamedPoint", Iterable]) -> "NamedPoint":
         try:
-            return NamedPoint((self.x+other.x, self.y+other.y), self.name)
+            return NamedPoint((self.x + other.x, self.y + other.y), self.name)
         except AttributeError:
-            return NamedPoint((self.x+other[0], self.y+other[1]))
+            return NamedPoint((self.x + other[0], self.y + other[1]))
 
-    def __sub__(self, other: Union['NamedPoint', Iterable]) -> 'NamedPoint':
+    def __sub__(self, other: Union["NamedPoint", Iterable]) -> "NamedPoint":
         try:
-            return NamedPoint((self.x-other.x, self.y-other.y), self.name)
+            return NamedPoint((self.x - other.x, self.y - other.y), self.name)
         except AttributeError:
-            return NamedPoint((self.x-other[0], self.y-other[1]))
+            return NamedPoint((self.x - other[0], self.y - other[1]))
 
 
-class NamedPixelColor(namedtuple('NamedPixelColor', ['name', 'color'])):
+class NamedPixelColor(namedtuple("NamedPixelColor", ["name", "color"])):
     ...
 
 
 def loopPause(pause=DEFAULT_LOOP_PAUSE):
-    '''
+    """
     等待迴圈延遲並檢查是否結束
-    '''
+    """
     sleep(pause)
     if exit_event.is_set():
-        raise ExitEventException('強制結束')
+        raise ExitEventException("強制結束")
 
 
 def getCursorPos() -> NamedPoint:
-    '''
+    """
     取得滑鼠座標
-    '''
-    return NamedPoint(win32api.GetCursorPos(), 'CursorPos')
+    """
+    return NamedPoint(win32api.GetCursorPos(), "CursorPos")
 
 
 def getPixel(point: NamedPoint) -> tuple[int, int, int]:
-    '''
+    """
     取得圖片座標像素顏色
 
     point: 座標
-    '''
-    try:
-        return pag.pixel(point.x, point.y)
-    except:
-        return None
+    """
+    return pag.pixel(point.x, point.y)
 
 
 def setPicPath(path: str) -> None:
-    '''
+    """
     設定圖片表路徑
 
     path: 圖片表路徑
-    '''
+    """
     global PIC_PATH, PIC
     if Path(path).is_absolute():
         PIC_PATH = path
@@ -103,38 +100,41 @@ def setPicPath(path: str) -> None:
         PIC_PATH = Path(path).resolve().as_posix()
 
     try:
-        with open(PIC_PATH, 'r') as picAddress:
+        with open(PIC_PATH, "r") as picAddress:
             PIC = json.load(picAddress)
     except FileNotFoundError:
-        raise Exception('找不到圖片表')
+        raise Exception("找不到圖片表")
 
 
-def find(*locations: Union[str, NamedPoint],
-         confidence: float = DEFALUT_CONFIDENCE,
-         centerPixelColor: tuple[NamedPixelColor] = None,
-         tolerance: int = 5
-         ) -> Union[NamedPoint, None]:
-    '''
+def find(
+    *locations: Union[str, NamedPoint],
+    confidence: float = DEFALUT_CONFIDENCE,
+    centerPixelColor: tuple[NamedPixelColor] = None,
+    tolerance: int = 5,
+) -> Union[NamedPoint, None]:
+    """
     將圖片名稱轉成座標 (如果傳入名稱有找到回傳座標,否則回傳None)
 
     *locations: 圖片名稱 或 座標
     confidence: 搜尋精準度
     centerPixelColor: 圖片中心像素顏色 tuple(NamedPixelColor(name, color), ...)
     tolerance: 容許誤差
-    '''
+    """
     # 尋找各個點
     point: NamedPoint = None
     for location in locations:
         if isinstance(location, str):
-            pagPoint = pag.locateCenterOnScreen(PIC[location], grayscale=True, confidence=confidence)
+            pagPoint = pag.locateCenterOnScreen(
+                PIC[location], grayscale=True, confidence=confidence
+            )
             if pagPoint is not None:
                 point = NamedPoint(pagPoint, location)
         elif isinstance(location, NamedPoint):
             point = location
-        elif type(location) == 'pyscreeze.Point':
+        elif type(location) == "pyscreeze.Point":
             point = NamedPoint((location.x, location.y))
         else:
-            raise Exception(f'參數型態錯誤({type(location)})')
+            raise Exception(f"參數型態錯誤({type(location)})")
 
         if point is not None:
             break
@@ -158,14 +158,14 @@ def find(*locations: Union[str, NamedPoint],
 
 
 def waitFind(*args, **kwargs) -> NamedPoint:
-    '''
+    """
     等待圖片出現
 
     *locations: 圖片名稱 或 座標
     confidence: 搜尋精準度
     centerPixelColor: 圖片中心像素顏色 tuple(NamedPixelColor(name, color), ...)
     tolerance: 容許誤差
-    '''
+    """
     while True:
         point = find(*args, **kwargs)
         if point:
@@ -177,14 +177,14 @@ def waitFind(*args, **kwargs) -> NamedPoint:
 
 
 def click(*locations, **kwargs) -> Union[NamedPoint, None]:
-    '''
+    """
     傳入pic_address中的名稱或是座標並按下
 
     *location: 圖片名稱 或 座標
     confidence: 搜尋精準度
     centerPixelColor: 圖片中心像素顏色 tuple(NamedPixelColor(name, color), ...)
     tolerance: 容許誤差
-    '''
+    """
     point = find(*locations, **kwargs)
 
     if point is None:
@@ -193,11 +193,13 @@ def click(*locations, **kwargs) -> Union[NamedPoint, None]:
     # 獲取原本滑鼠位置
     currentMouse = win32gui.GetCursorPos()
     if FAILSAFE and currentMouse[0] == currentMouse[1] == 0:
-        raise Exception('強制結束')
+        raise Exception("強制結束")
 
     # 點擊偵測到的圖片
     win32api.SetCursorPos((point.x, point.y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    win32api.mouse_event(
+        win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_LEFTUP, 0, 0
+    )
     sleep(0.05)
 
     # 滑鼠回原本位置
@@ -206,8 +208,10 @@ def click(*locations, **kwargs) -> Union[NamedPoint, None]:
     return point
 
 
-def waitClick(*locations, delay: float = 0, wait: float = -1, **kwargs) -> Union[NamedPoint, None]:
-    '''
+def waitClick(
+    *locations, delay: float = 0, wait: float = -1, **kwargs
+) -> Union[NamedPoint, None]:
+    """
     等待並按下,時間內有按下回圖片Point,超時則回傳None
 
     *locations: 圖片名稱 或 座標
@@ -216,10 +220,10 @@ def waitClick(*locations, delay: float = 0, wait: float = -1, **kwargs) -> Union
     confidence: 搜尋精準度
     centerPixelColor: 圖片中心像素顏色 tuple(NamedPixelColor(name, color), ...)
     tolerance: 容許誤差
-    '''
+    """
     # 計算超時時間
     if wait >= 0:
-        timeout = perf_counter()+wait
+        timeout = perf_counter() + wait
 
     # 循環搜尋
     while True:
--- script.py
+++ script.py
@@ -6,25 +6,24 @@ import pyautogui as pag
 import operate as op
 from operate import loopPause
 
-
 # 預設圖片表路徑
-DEFAULT_PIC_PATH: str = 'pic_address.json'
+DEFAULT_PIC_PATH: str = "pic_address.json"
 op.setPicPath(DEFAULT_PIC_PATH)
 
 
 def waitLoading() -> None:
-    '''
+    """
     等待Loading
-    '''
-    timeout = perf_counter()+1
-    while not op.find('loading'):
+    """
+    timeout = perf_counter() + 1
+    while not op.find("loading"):
         if perf_counter() > timeout:
             break
 
         loopPause()
 
-    timeout = perf_counter()+1
-    while op.find('loading'):
+    timeout = perf_counter() + 1
+    while op.find("loading"):
         if perf_counter() > timeout:
             break
 
@@ -32,165 +31,171 @@ def waitLoading() -> None:
 
 
 def waitBattleEnd() -> None:
-    '''
+    """
     等待戰鬥結束
-    '''
-    while not op.find('fighting'):
+    """
+    while not op.find("fighting"):
         loopPause()
 
-    while op.find('fighting'):
+    while op.find("fighting"):
         loopPause()
 
 
 def select_team(team: str) -> None:
-    '''
+    """
     選擇隊伍
 
     team: 隊伍名稱 (
-        team_battle_arena_normal, 
-        team_battle_arena_ex, 
+        team_battle_arena_normal,
+        team_battle_arena_ex,
         team_event,
         team_cabbage
         )
-    '''
-    if not op.find('go'):
+    """
+    if not op.find("go"):
         return False
 
-    teamNumber = {'team_battle_arena_nm': 0,
-                  'team_battle_arena_ex': 1,
-                  'team_event': 2,
-                  'team_cabbage': 3
-                  }
+    teamNumber = {
+        "team_battle_arena_nm": 0,
+        "team_battle_arena_ex": 1,
+        "team_event": 2,
+        "team_cabbage": 3,
+    }
     c = 0
     for t in teamNumber.keys():
         teamNamePoint = op.find(t)
         if teamNamePoint:
-            c = teamNumber[t]-teamNumber[team]
+            c = teamNumber[t] - teamNumber[team]
             break
     else:
-        print('沒有找到隊伍名稱')
+        print("沒有找到隊伍名稱")
 
     while True:
         if c == 0:
             break
         elif c > 0:
-            op.click(teamNamePoint+(-1090, 240))
+            op.click(teamNamePoint + (-1090, 240))
             c -= 1
         elif c < 0:
-            op.click(teamNamePoint+(110, 240))
+            op.click(teamNamePoint + (110, 240))
             c += 1
 
         loopPause()
 
 
 def goAdventure() -> bool:
-    '''
+    """
     移動到戰鬥
-    '''
-    if op.find('adventure_0'):
-        op.click('adventure_0')
+    """
+    if op.find("adventure_0"):
+        op.click("adventure_0")
         return True
-    elif op.find('adventure_1'):
-        op.click('adventure_1')
+    elif op.find("adventure_1"):
+        op.click("adventure_1")
         return True
 
     return False
 
 
 def battleArenaLoop() -> bool:
-    '''
+    """
     刷競技場
-    '''
+    """
+
     def battleArena(mode):
-        if op.waitClick(f'battle_arena_{mode}_0', wait=0):
+        if op.waitClick(f"battle_arena_{mode}_0", wait=0):
             waitLoading()
 
-        if op.find('no_challenge'):
+        if op.find("no_challenge"):
             return False
 
-        op.waitClick('ok', wait=0)
-        op.waitClick('challenge')
-        if mode == 'normal':
-            op.waitClick('advanced', delay=0.5)
-        elif mode == 'ex':
-            op.waitClick('battle_arena_ex', delay=0.5)
+        op.waitClick("ok", wait=0)
+        op.waitClick("challenge")
+        if mode == "normal":
+            op.waitClick("advanced", delay=0.5)
+        elif mode == "ex":
+            op.waitClick("battle_arena_ex", delay=0.5)
 
-        op.waitClick('ready', delay=0.5)
-        select_team(f'team_battle_arena_{mode}')
-        op.waitClick('go')
+        op.waitClick("ready", delay=0.5)
+        select_team(f"team_battle_arena_{mode}")
+        op.waitClick("go")
         while True:
             waitLoading()
             refresh = False
             while True:
-                if op.find('refresh'):
+                if op.find("refresh"):
                     refresh = True
                     break
 
-                if op.find('next'):
+                if op.find("next"):
                     refresh = False
                     break
 
                 loopPause()
 
-            if op.waitClick('refresh', wait=1, delay=1):
-                print('刷新')
+            if op.waitClick("refresh", wait=1, delay=1):
+                print("刷新")
                 sleep(1)
                 pag.click()
                 sleep(1)
                 pag.click()
 
-            op.waitClick('next', delay=2)
-            op.waitClick('next', delay=1.5)
-            if refresh and mode == 'normal':
-                op.waitClick('next', delay=1.5)
+            op.waitClick("next", delay=2)
+            op.waitClick("next", delay=1.5)
+            if refresh and mode == "normal":
+                op.waitClick("next", delay=1.5)
 
-            op.waitClick('ok', wait=1)
-            op.waitClick('again')
-            if not op.waitClick('ok', wait=0.5):
+            op.waitClick("ok", wait=1)
+            op.waitClick("again")
+            if not op.waitClick("ok", wait=0.5):
                 break
 
-        op.waitClick('next')
+        op.waitClick("next")
         return True
 
     goAdventure()  # 戰鬥
-    op.waitClick('battle_arena')  # 競技場
+    op.waitClick("battle_arena")  # 競技場
     waitLoading()
 
-    battleArena('normal')
-    battleArena('ex')
+    battleArena("normal")
+    battleArena("ex")
 
 
 def eventBossLoop(firstDelay: Union[str, int]) -> None:  # 活動迴圈
-    '''
+    """
     刷活動BOSS
 
     firstDelay: 第一次延遲
-    '''
+    """
+
     def eventBoss() -> int:  # 跑一次活動
-        '''
+        """
         跑一次活動(回傳 1:正常運行, 0:需重製預設延遲, -1:無法繼續)
-        '''
-        button = op.waitFind('next', 'go')
-        if button.name == 'go':
+        """
+
+        button = op.waitFind("next", "go", "dead_again")
+        if button.name == "go":
             op.click(button.name)
             return 0
         else:
-            againColor = op.NamedPixelColor('again', (157, 149, 140))
-            click = op.waitClick('again', 'dead_again', wait=2, centerPixelColor=(againColor,))
+            againColor = op.NamedPixelColor("again", (157, 149, 140))
+            clicked = op.waitClick(
+                "again", "dead_again", wait=2, centerPixelColor=(againColor,)
+            )
 
-        if click is None:
-            op.waitClick('next', wait=1)
+        if clicked is None:
+            op.waitClick("next", wait=1)
             return -1
 
-        op.waitClick('ok')
-        if click.name == 'dead_again':
+        op.waitClick("ok")
+        if clicked.name == "dead_again":
             return 0
 
         return 1
 
     DEFAULT_DELAY = 60  # 預設平均延遲
 
-    select_team('team_event')
+    select_team("team_event")
     try:
         delay = float(firstDelay)
     except ValueError:
@@ -200,7 +205,7 @@ def eventBossLoop(firstDelay: Union[str, int]) -> None:  # 活動迴圈
     while True:
         print(f"延遲{delay:.1f}秒")
         sleep(delay)
-        print('準備下一場\n')
+        print("準備下一場\n")
         readyTime = perf_counter()
 
         eventBossRtn = eventBoss()
@@ -213,53 +218,53 @@ def eventBossLoop(firstDelay: Union[str, int]) -> None:  # 活動迴圈
             break
 
         # 延遲時間計算
-        waited = perf_counter()-readyTime
+        waited = perf_counter() - readyTime
         if waited <= 2.5:
             change = -5
         else:
-            change = (perf_counter()-readyTime)/2
+            change = (perf_counter() - readyTime) / 2
 
         delay += change
         print(f"等待{waited:.1f}秒")
-        print(f'延遲變更{change:.1f}秒')
+        print(f"延遲變更{change:.1f}秒")
 
-    op.waitClick('next', delay=1)
-    op.waitClick('back', delay=0.5)
+    op.waitClick("next", delay=1)
+    op.waitClick("back", delay=0.5)
 
 
 def eventAdventureLoop() -> None:
-    '''
+    """
     刷新活動關卡
-    '''
+    """
     while True:
-        op.waitClick('next', delay=1.5)
+        op.waitClick("next", delay=1.5)
         sleep(1)
-        op.waitClick('next', delay=2)
+        op.waitClick("next", delay=2)
         waitLoading()
         waitLoading()
         waitLoading()
-        op.waitClick('watch_later', wait=1)
-        if not op.waitClick('ready'):
+        op.waitClick("watch_later", wait=1)
+        if not op.waitClick("ready"):
             break
 
-        select_team('team_event')
-        op.waitClick('go')
+        select_team("team_event")
+        op.waitClick("go")
 
 
 def goHome() -> None:
-    '''
+    """
     回主畫面
-    '''
-    if op.find('home_0'):
-        op.waitClick('home_0')
-    elif op.find('home_1'):
-        op.waitClick('home_1')
+    """
+    if op.find("home_0"):
+        op.waitClick("home_0")
+    elif op.find("home_1"):
+        op.waitClick("home_1")
 
 
 def goJob() -> None:
-    '''
+    """
     到打工畫面
-    '''
-    op.waitClick('job')
-    op.waitClick('all_receive')
-    op.waitClick('ok')
+    """
+    op.waitClick("job")
+    op.waitClick("all_receive")
+    op.waitClick("ok")
